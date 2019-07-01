09907a1647674eb6daafbdec620aa6b3fbd2eb2c
watarui <wataruby@gmail.com>
2019-07-01 23:55:18 +0900

    Complete kyuyo.ml

--- src/kyuyo.ml
+++ src/kyuyo.ml
@@ -1,6 +1,12 @@
+(* 時給: 円 *)
+let jikyu = 950
+
+(* 基本給: 円 *)
+let kihonkyu = 100
+
 (* 働いた時間xに応じたアルバイト代を計算する *)
 (* kyuyo : int -> int *)
-let kyuyo x = 0
+let kyuyo x = kihonkyu + x * jikyu
 
 (* test *)
 let test1 = kyuyo 25 = 23850
@@ -9,8 +15,10 @@ let test3 = kyuyo 31 = 29550
 
 (*
 # #use "src/kyuyo.ml" ;;
-val kyuyo : 'a -> int = <fun>
-val test1 : bool = false
-val test2 : bool = false
-val test3 : bool = false
+val jikyu : int = 950
+val kihonkyu : int = 100
+val kyuyo : int -> int = <fun>
+val test1 : bool = true
+val test2 : bool = true
+val test3 : bool = true
 *)
