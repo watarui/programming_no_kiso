0215eb5f40f6cbf3f314d76ba84420e2e207f255
watarui <wataruby@gmail.com>
2019-07-03 21:36:49 +0900

    Fix syntax

--- src/chapter8/okozukai_t.ml
+++ src/chapter8/okozukai_t.ml
@@ -6,23 +6,23 @@ type okozukai_t = {
     date : string; (* 日付 *)
 }
 
-type okozukai1 = {
-    name : "ame";
-    price : 100;
-    place : "lowson";
-    date : "20200701";
+let okozukai1 = {
+    name = "ame";
+    price = 100;
+    place = "lowson";
+    date = "20200701";
 }
 
-type okozukai2 = {
-    name : "gum";
-    price : 100;
-    place : "seven";
-    date : "20200702";
+let okozukai2 = {
+    name = "gum";
+    price = 100;
+    place = "seven";
+    date = "20200702";
 }
 
-type okozukai3 = {
-    name : "soba";
-    price : 330;
-    place : "family mart";
-    date : "20200703";
+let okozukai3 = {
+    name = "soba";
+    price = 330;
+    place = "family mart";
+    date = "20200703";
 }
--- src/chapter8/person_t.ml
+++ src/chapter8/person_t.ml
@@ -1,28 +1,32 @@
 (* 各人の情報を格納するレコード *)
 type person_t = {
+    name : string; (* 名前 *)
     height : float; (* 身長m *)
     weight : float; (* 体重kg *)
     birthday : string; (* 誕生日（月と日） *)
     blood : string; (* 血液型 *)
 }
 
-type person1 = {
-    height : 180.0;
-    weight : 77.7;
-    birthday : "0624";
-    blood : "AB";
+let person1 = {
+    name = "oka";
+    height = 180.0;
+    weight = 77.7;
+    birthday = "0624";
+    blood = "AB";
 }
 
-type person2 = {
-    height : 166.6;
-    weight : 66.6;
-    birthday : "1111";
-    blood : "O";
+let person2 = {
+    name = "oda";
+    height = 166.6;
+    weight = 66.6;
+    birthday = "1111";
+    blood = "O";
 }
 
-type person3 = {
-    height : 175.4;
-    weight : 81.0;
-    birthday : "0229";
-    blood : "A";
+let person3 = {
+    name = "ota";
+    height = 175.4;
+    weight = 81.0;
+    birthday = "0229";
+    blood = "A";
 }
