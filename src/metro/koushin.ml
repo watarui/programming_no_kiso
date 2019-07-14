29da4cc8dcd2c0a12ba67821fe2a5ddb52084a21
watarui <wataruby@gmail.com>
2019-07-14 22:24:40 +0900

    ダイクストラ法

--- src/metro/koushin.ml
+++ src/metro/koushin.ml
@@ -7,14 +7,14 @@ let eki3 = {namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]}
 let eki4 = {namae="後楽園"; saitan_kyori = infinity; temae_list = []}
 
 (* 目的 : 直前に確定した駅pと未確定の駅のリストvを受け取り、更新処理後の未確定の駅のリストを返す *)
-(* koushin : eki_t -> eki_t list -> eki_t list *)
-let koushin p v =
+(* koushin : eki_t -> eki_t list -> ekikan_t list -> eki_t list *)
+let koushin p v lst =
   match p with
     {namae = pn; saitan_kyori = ps; temae_list = pt} ->
     List.map (fun q ->
         match q with
           {namae = qn; saitan_kyori = qs; temae_list = qt} ->
-          let kyori = get_ekikan_kyori pn qn global_ekikan_list in
+          let kyori = get_ekikan_kyori pn qn lst in
           if (kyori = infinity) || (ps +. kyori >= qs)
           then q
           (* 最短距離がp経由のほうが短い場合 *)
@@ -22,6 +22,6 @@ let koushin p v =
       )
       v
 
-let test1 = koushin eki2 [] = []
-let test2 = koushin eki2 [eki1; eki2; eki3; eki4] =
+let test1 = koushin eki2 [] global_ekikan_list = []
+let test2 = koushin eki2 [eki1; eki2; eki3; eki4] global_ekikan_list =
             [{namae="池袋"; saitan_kyori = 3.0; temae_list = ["池袋"; "新大塚"; "茗荷谷"]}; eki2; eki3; eki4]
