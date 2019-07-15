50e882ee5eed7a22da431090e23759c2bdb61200
watarui <wataruby@gmail.com>
2019-07-16 01:18:58 +0900

    例外処理を排除

--- src/chapter18/price.ml
+++ src/chapter18/price.ml
@@ -9,8 +9,8 @@ let yaoya_list = [("トマト", 300); ("たまねぎ", 200); ("にんじん", 15
 (* price : string -> (string * int) list -> int option *)
 let rec price item lst =
   match lst with
-  | [] -> None
-  | (yasai, nedan) :: rest -> if item = yasai then Some (nedan) else price item rest
+  | [] -> 0
+  | (yasai, nedan) :: rest -> if item = yasai then nedan else price item rest
 
-let test1 = price "たまねぎ" yaoya_list = Some (200)
-let test2 = price "じゃがいも" yaoya_list = None
+let test1 = price "たまねぎ" yaoya_list = 200
+let test2 = price "じゃがいも" yaoya_list = 0
--- src/chapter18/total_price.ml
+++ src/chapter18/total_price.ml
@@ -3,21 +3,10 @@
 (* 目的 : yasai_list を買ったときの値段の合計を調べる *)
 (* total_price : string list -> (string * int) list -> int *)
 let rec total_price yasai_list yaoya_list =
-  (* int option 型を返す *)
-  let rec hojo yasai_list =
-    match yasai_list with
-    | [] -> Some (0)
-    | first :: rest ->
-      match price first yaoya_list with
-      | None -> None (* 一つでも買えない野菜がある場合は None を返す *)
-      | Some (p) ->
-        match hojo rest with
-        | None -> None
-        | Some (q) -> Some (p + q) in
-  (* int 型に変換 *)
-  match hojo yasai_list with
-  | None -> 0
-  | Some (p) -> p
+  match yasai_list with
+  | [] -> 0
+  | first :: rest ->
+    price first yaoya_list + total_price rest yaoya_list
 
 let test1 = total_price ["たまねぎ"; "にんじん"] yaoya_list = 350
-let test2 = total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list = 0
+let test2 = total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list <> 0 (* すべての野菜が買える前提なので 0 にはならない *)
