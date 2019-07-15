cdc10ab80a8b40b1a13ecccb02d10c3af243cc1b
watarui <wataruby@gmail.com>
2019-07-16 01:43:27 +0900

    例外処理構文（raise, exception, try ... with）

--- src/chapter18/price.ml
+++ src/chapter18/price.ml
@@ -4,13 +4,16 @@ type ' option = None
               | Some of 'a
 *)
 
+(* 売り切れを示す例外 *)
+exception Urikire
+
 let yaoya_list = [("トマト", 300); ("たまねぎ", 200); ("にんじん", 150); ("ほうれん草", 200)]
 
-(* price : string -> (string * int) list -> int option *)
+(* price : string -> (string * int) list -> int *)
 let rec price item lst =
   match lst with
-  | [] -> 0
+  | [] -> raise Urikire (* 見つからない場合は Urikire という例外を投げる *)
   | (yasai, nedan) :: rest -> if item = yasai then nedan else price item rest
 
 let test1 = price "たまねぎ" yaoya_list = 200
-let test2 = price "じゃがいも" yaoya_list = 0
+(* let test2 = price "じゃがいも" yaoya_list = 0 *)
--- src/chapter18/total_price.ml
+++ src/chapter18/total_price.ml
@@ -2,11 +2,14 @@
 
 (* 目的 : yasai_list を買ったときの値段の合計を調べる *)
 (* total_price : string list -> (string * int) list -> int *)
-let rec total_price yasai_list yaoya_list =
-  match yasai_list with
-  | [] -> 0
-  | first :: rest ->
-    price first yaoya_list + total_price rest yaoya_list
+let total_price yasai_list yaoya_list =
+  let rec hojo yasai_list =
+    match yasai_list with
+    | [] -> 0
+    | first :: rest ->
+      price first yaoya_list + hojo rest in
+  try hojo yasai_list with
+  | Urikire -> 0
 
 let test1 = total_price ["たまねぎ"; "にんじん"] yaoya_list = 350
-let test2 = total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list <> 0 (* すべての野菜が買える前提なので 0 にはならない *)
+let test2 = total_price ["たまねぎ"; "じゃがいも"; "にんじん"] yaoya_list = 0
